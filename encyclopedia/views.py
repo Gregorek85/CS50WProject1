f2d56f4e27b1497761ebb4884c98cbfceac76aa9
Gregorek85 <grerad@gmail.com>
2020-08-17 22:38:30 +0200

    entry pages are loaded by visiting wiki/entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, entry):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "entry_content" : markdown2.markdown(util.get_entry(entry))
     })
 
